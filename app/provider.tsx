03f3b07c4e9821294e2d4b8e5c489e125f3f3275
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2026-01-11 03:10:48 +0700

    refactor(provider): replace OnchainKit and Wagmi providers with MiniKitProvider

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,11 +1,10 @@
 "use client";
 
+import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
 import type { ReactNode } from "react";
 import { MiniAppProvider } from "@/components/providers/miniapp-provider";
-import { OnchainKitProvider } from "@/components/providers/onchainkit-provider";
 import { SafeAreaProvider } from "@/components/providers/safe-area-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import { WagmiProvider } from "@/components/providers/wagmi-provider";
 
 export function Provider({
   children,
@@ -19,13 +18,11 @@ export function Provider({
       enableSystem
       storageKey="theme"
     >
-      <WagmiProvider>
-        <OnchainKitProvider>
-          <MiniAppProvider>
-            <SafeAreaProvider>{children}</SafeAreaProvider>
-          </MiniAppProvider>
-        </OnchainKitProvider>
-      </WagmiProvider>
+      <MiniKitProvider enabled={true}>
+        <MiniAppProvider>
+          <SafeAreaProvider>{children}</SafeAreaProvider>
+        </MiniAppProvider>
+      </MiniKitProvider>
     </ThemeProvider>
   );
 }
