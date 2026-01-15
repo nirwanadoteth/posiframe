3aa9989818050a7708d6f8e21839a19392c5213f
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2026-01-16 04:05:25 +0700

    refactor(progress): improve component structure and streamline imports

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,31 +1,27 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
+import { Indicator, Root } from "@radix-ui/react-progress";
+import type { ComponentProps } from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Progress({
-  className,
-  value,
-  ...props
-}: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+function Progress({ className, value, ...props }: ComponentProps<typeof Root>) {
   return (
-    <ProgressPrimitive.Root
-      data-slot="progress"
+    <Root
       className={cn(
-        "bg-primary/20 relative h-2 w-full overflow-hidden rounded-full",
+        "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
         className
       )}
+      data-slot="progress"
       {...props}
     >
-      <ProgressPrimitive.Indicator
+      <Indicator
+        className="h-full w-full flex-1 bg-primary transition-all"
         data-slot="progress-indicator"
-        className="bg-primary h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
-    </ProgressPrimitive.Root>
-  )
+    </Root>
+  );
 }
 
-export { Progress }
+export { Progress };
