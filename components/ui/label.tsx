d796f49918d13917e0249d3af5035bc874e6d787
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2026-01-25 15:59:00 +0700

    refactor(label): replace Root component with native label and adjust styling for consistency

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { Root } from "@radix-ui/react-label";
 import type { ComponentProps } from "react";
 
 import { cn } from "@/lib/utils";
 
-function Label({ className, ...props }: ComponentProps<typeof Root>) {
+function Label({ className, ...props }: ComponentProps<"label">) {
   return (
-    <Root
+    <label
       className={cn(
-        "flex select-none items-center gap-2 font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50",
+        "flex select-none items-center gap-2 font-medium text-xs/relaxed leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50",
         className
       )}
       data-slot="label"
