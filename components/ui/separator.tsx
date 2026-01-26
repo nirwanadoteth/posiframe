9d34fa5a06871fe9b9c5cc4a0cd07ad4fcad8a4c
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2026-01-27 01:48:37 +0700

    refactor(separator): replace Radix UI with base-ui components and simplify structure

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,24 +1,21 @@
 "use client";
 
-import { Root } from "@radix-ui/react-separator";
-import type { ComponentProps } from "react";
+import { Separator as SeparatorPrimitive } from "@base-ui/react/separator";
 
 import { cn } from "@/lib/utils";
 
 function Separator({
   className,
   orientation = "horizontal",
-  decorative = true,
   ...props
-}: ComponentProps<typeof Root>) {
+}: SeparatorPrimitive.Props) {
   return (
-    <Root
+    <SeparatorPrimitive
       className={cn(
-        "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px",
+        "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px data-[orientation=vertical]:self-stretch",
         className
       )}
       data-slot="separator"
-      decorative={decorative}
       orientation={orientation}
       {...props}
     />
